3a429fc4f5385f17b250c3918cb0f362831d9f48
Arie Trost <atrost@marketron.com>
2018-04-10 13:19:14 -0700

    removed file extention fot template referencing

--- src/hello.js
+++ src/hello.js
@@ -1,5 +1,5 @@
 import Backbone from 'backbone';
-import HelloTempl from './hello-tmpl.ejs';
+import HelloTempl from './hello-tmpl';
 
 // HelloTempl is a module that contains a precompiled version of the hello-templ.ejs.
 // If using Backbone.js, you can render it like so:
